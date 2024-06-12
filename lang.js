956076ffb289872e227ab3df9a68ce73bb898cc0
fedelamagna <59378200+fedelamagna@users.noreply.github.com>
2024-06-12 09:28:36 -0300

    Update lang.js

--- lang.js
+++ lang.js
@@ -10,9 +10,11 @@ function runlang(){
 if (language === 'en') {
   document.querySelectorAll('.es').forEach(elem => elem.style.display = 'none');
   document.querySelectorAll('.en').forEach(elem => elem.style.display = 'inline');
+  document.title = 'Artificial Intelligence for Executives: Course Syllabus';
 } else {
   document.querySelectorAll('.en').forEach(elem => elem.style.display = 'none');
   document.querySelectorAll('.es').forEach(elem => elem.style.display = 'inline');
+  document.title = 'Inteligencia Artificial para Ejecutivos: Temario del Curso';
 }}
 language = 'en';
 runlang();
